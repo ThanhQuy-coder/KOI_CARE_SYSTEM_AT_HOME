3f1217b5c8a69c2ee72ccb89958cdb8f9ab89d68
hongnhan1 <hongnhan210127@gmail.com>
2024-11-01 21:52:38 +0700

    thêm lớp dbcontext

---
+++ KoiCareSystemAtHome.WebApplication/Data/YourDbContext.cs
@@ -0,0 +1,14 @@
+﻿using KoiCareSystemAtHome.Repositories.Entities;
+using Microsoft.EntityFrameworkCore;
+
+public class YourDbContext : DbContext
+{
+    public YourDbContext(DbContextOptions<YourDbContext> options) : base(options)
+    {
+    }
+
+    // Định nghĩa các DbSet cho các bảng trong cơ sở dữ liệu
+    public DbSet<Product> Products { get; set; }
+    public DbSet<User> Users { get; set; }
+    // Thêm các DbSet khác cho các bảng khác
+}
