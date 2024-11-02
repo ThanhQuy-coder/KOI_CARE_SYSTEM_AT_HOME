0c678b6f5c37043c9a0ab32aca0ffc017eacf532
hongnhan1 <hongnhan210127@gmail.com>
2024-11-02 12:22:59 +0700

    sửa chính tả

---
+++ KoiCareSystemAtHome.Repositories/Entities/KoiFish.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace KoiCareSystemAtHome.Repositories.Entities;
+
+public partial class KoiFish
+{
+    public string? PondId { get; set; }
+
+    public string FishId { get; set; } = null!;
+
+    public string NameFish { get; set; } = null!;
+
+    public string ImageFish { get; set; } = null!;
+
+    public string BodyShape { get; set; } = null!;
+
+    public int AgeFish { get; set; }
+
+    public double Size { get; set; }
+
+    public double WeightFish { get; set; }
+
+    public string Gender { get; set; } = null!;
+
+    public string Breed { get; set; } = null!;
+
+    public string Origin { get; set; } = null!;
+
+    public double Price { get; set; }
+
+    public int FishLocation { get; set; }
+
+    public virtual Pond? Pond { get; set; }
+}
