d96af8607d439fb099a7ed455eb1bc24cd798eee
hongnhan1 <hongnhan210127@gmail.com>
2024-11-02 12:02:05 +0700

    thêm lớp Article

---
+++ KoiCareSystemAtHome.Repositories/Entities/Article.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KoiCareSystemAtHome.Repositories.Entities
+{
+    public class Article
+        {
+            public int ArticleId { get; set; } // ID của bài viết
+            public string Title { get; set; } // Tiêu đề bài viết
+            public string Content { get; set; } // Nội dung bài viết
+            public DateTime PublishDate { get; set; } // Ngày xuất bản
+            public int CategoryId { get; set; } // ID của danh mục
+            public Category Category { get; set; } // Đối tượng danh mục
+            public List<Comment> Comments { get; set; } // Danh sách bình luận
+        }
+    }
+
