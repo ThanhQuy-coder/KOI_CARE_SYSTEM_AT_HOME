839a5b58b3f4c0b6aee66ce8be99b897d4770d05
hongnhan1 <hongnhan210127@gmail.com>
2024-11-03 13:16:35 +0700

    thêm razor page và page model cho categories

---
+++ KoiCareSystemAtHome.WebApplication/Pages/Categories/Index.cshtml.cs
@@ -0,0 +1,23 @@
+using KoiCareSystemAtHome.Repositories.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace KoiCareSystemAtHome.WebApplication.Pages.Categories
+{
+    public class CategoriesModel : PageModel
+    {
+        private readonly YourDbContext _context;
+
+        public CategoriesModel(YourDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Category> Categories { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            Categories = await _context.Categories.ToListAsync();
+        }
+    }
