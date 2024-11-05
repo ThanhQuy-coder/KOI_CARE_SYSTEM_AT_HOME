f2ea78ba7960a54508242736b5ada6a4bd44de56
hongnhan1 <hongnhan210127@gmail.com>
2024-11-05 14:52:09 +0700

    thêm file repo

---
+++ KoiCareSystemAtHome.Repositories/Repositories/GenericRepository.cs
@@ -0,0 +1,52 @@
+﻿using KoiCareSystemAtHome.Repositories.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KoiCareSystemAtHome.Repositories.Repositories
+{
+    public class GenericRepository<T> : IRepository<T> where T : class
+    {
+        private readonly KoiCareSystemAtHomeContext _context;
+
+        public GenericRepository(KoiCareSystemAtHomeContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _context.Set<T>().ToListAsync();
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await _context.Set<T>().FindAsync(id);
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            await _context.Set<T>().AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            _context.Set<T>().Update(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var entity = await GetByIdAsync(id);
+            if (entity != null)
+            {
+                _context.Set<T>().Remove(entity);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
