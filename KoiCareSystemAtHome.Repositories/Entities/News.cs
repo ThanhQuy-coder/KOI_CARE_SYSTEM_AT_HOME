c2f1d105caf62334b80005d14e1b31b2eb724a09
hongnhan1 <hongnhan210127@gmail.com>
2024-11-08 15:12:28 +0700

    chỉ định postID là khoá chính

--- KoiCareSystemAtHome.Repositories/Entities/News.cs
+++ KoiCareSystemAtHome.Repositories/Entities/News.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 
 namespace KoiCareSystemAtHome.Repositories.Entities;
 
 public partial class News
 {
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int PostId { get; set; }
 
     public string Author { get; set; } = null!;
