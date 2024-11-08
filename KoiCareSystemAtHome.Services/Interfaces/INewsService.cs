29055c43fce1d96d46b0eb1e23c7d1c818c01d8a
hongnhan1 <hongnhan210127@gmail.com>
2024-11-08 15:13:55 +0700

    thêm inews service và news service

---
+++ KoiCareSystemAtHome.Services/Interfaces/INewsService.cs
@@ -0,0 +1,18 @@
+﻿using KoiCareSystemAtHome.Repositories.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace KoiCareSystemAtHome.Services.Interfaces
+{
+    public interface INewsService
+    {
+        IEnumerable<News> GetAllNews();
+        News GetNewsById(int id);
+        void AddNews(News news);
+        void UpdateNews(News news);
+        void DeleteNews(int id);
+    }
+}
+
---
+++ KoiCareSystemAtHome.Services/Services/NewsService.cs
@@ -0,0 +1,64 @@
+﻿using KoiCareSystemAtHome.Repositories.Entities;
+using KoiCareSystemAtHome.Repositories.Interfaces;
+using KoiCareSystemAtHome.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace KoiCareSystemAtHome.Services.Services
+{
+    public class NewsService : INewsService
+    {
+        private readonly KoiCareSystemAtHomeContext _context;
+
+        public NewsService(KoiCareSystemAtHomeContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<News> GetAllNews()
+        {
+            return _context.News.ToList();
+        }
+
+        public News GetNewsById(int id)
+        {
+            return _context.News.Find(id);
+        }
+
+        public void AddNews(News news)
+        {
+            _context.News.Add(news);
+            _context.SaveChanges();
+        }
+
+        public void UpdateNews(News news)
+        {
+            _context.Entry(news).State = EntityState.Modified;
+            _context.SaveChanges();
+        }
+
+        public void DeleteNews(int id)
+        {
+            var news = _context.News.Find(id);
+            if (news != null)
+            {
+                _context.News.Remove(news);
+                _context.SaveChanges();
+            }
+        }
+    }
+}
+
+
+
+
+
+     
+   
+
+
