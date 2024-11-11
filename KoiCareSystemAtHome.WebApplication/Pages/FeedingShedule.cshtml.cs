1a76be48267fccda7bb85c8360a677c52c000be2
Bình Dương Ngọc <binhdn4111@ut.edu.vn>
2024-11-11 10:08:31 +0700

    thay doi WebApplication

---
+++ KoiCareSystemAtHome.WebApplication/Pages/FeedingShedule.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace KoiCareSystemAtHome.WebApplication.Pages
+{
+    public class FeedingSheduleModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ KoiCareSystemAtHome.WebApplication/Pages/SaltCalculation.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace KoiCareSystemAtHome.WebApplication.Pages
+{
+    public class SaltCalculationModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
