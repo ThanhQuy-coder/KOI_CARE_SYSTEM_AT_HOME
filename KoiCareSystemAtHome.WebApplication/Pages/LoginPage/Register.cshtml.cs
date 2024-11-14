8957d44e7846a5f088d233d6666739fb65f64cfd
Thanh Quy <quyht2908@gmail.com>
2024-11-14 16:38:04 +0700

    Đổi tên ModelUser thành LoginPage

---
+++ KoiCareSystemAtHome.WebApplication/Pages/LoginPage/Register.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace KoiCareSystemAtHome.WebApplication.Pages.LoginPage
+{
+    public class RegisterModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ KoiCareSystemAtHome.WebApplication/Pages/LoginPage/forgotPassword.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace KoiCareSystemAtHome.WebApplication.Pages.LoginPage
+{
+    public class forgotPasswordModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
