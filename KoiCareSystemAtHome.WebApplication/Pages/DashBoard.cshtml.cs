f68aace8ef292b8f62bc0b457c72e62178849ff2
Thanh Quy <quyht2908@gmail.com>
2024-11-16 01:43:30 +0700

    Thêm biểu đồ ví dụ trong dashboard

--- KoiCareSystemAtHome.WebApplication/Pages/DashBoard.cshtml.cs
+++ KoiCareSystemAtHome.WebApplication/Pages/DashBoard.cshtml.cs
@@ -1,3 +1,6 @@
+﻿using KoiCareSystemAtHome.Repositories.Entities;
+using KoiCareSystemAtHome.Repositories.Interfaces;
+using KoiCareSystemAtHome.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +8,32 @@ namespace KoiCareSystemAtHome.WebApplication.Pages
 {
     public class DashBoardModel : PageModel
     {
-        public void OnGet()
+        private readonly IKoiFishService _koifishService;
+
+        public DashBoardModel(IKoiFishService koifishService)
         {
+            _koifishService = koifishService;
+        }
+
+        public List<KoiFish> KoiData { get; set; }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            // Lấy UserId từ Session
+            var userId = HttpContext.Session.GetString("UserId");
+
+            // Kiểm tra nếu không có UserId trong Session (nghĩa là chưa đăng nhập)
+            if (string.IsNullOrEmpty(userId))
+            {
+                // Chuyển hướng người dùng về trang đăng nhập
+                return RedirectToPage("/LoginPage/Index");
+            }
+
+            // Nếu có UserId, tiếp tục xử lý
+            // Truyền UserId vào ViewData để có thể sử dụng trong Razor Page nếu cần
+            ViewData["UserId"] = userId;
+            return Page();
         }
     }
 }
+
