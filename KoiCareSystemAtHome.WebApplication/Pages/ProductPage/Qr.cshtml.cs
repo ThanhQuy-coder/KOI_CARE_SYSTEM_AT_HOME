006382f9345290086132219b3801ce6ba50a7fe5
Sakayanagi-Arisuu <quynguyen1404.dz@gmail.com>
2024-11-16 23:06:24 +0700

    cải tiến chức năng thnah toán

---
+++ KoiCareSystemAtHome.WebApplication/Pages/ProductPage/Qr.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace KoiCareSystemAtHome.WebApplication.Pages.ProductPage
+{
+    public class QrModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
